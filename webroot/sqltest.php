7f0a925a1bc90ab2c8c2a779728c1c008f89ad56
Mohamed Ashraf Ali <ec20930@qmul.ac.uk>
2021-04-11 14:28:10 +0100

    test

--- webroot/sqltest.php
+++ webroot/sqltest.php
@@ -12,6 +12,13 @@
 </html>
 
 <?php
+
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbport = getenv("MYSQL_SERVICE_PORT");
+$dbuser = getenv("DATABASE_USER");
+$dbpwd = getenv("DATABASE_PASSWORD");
+$dbname = getenv("DATABASE_NAME");
+
 $conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 
 // Checks connection
